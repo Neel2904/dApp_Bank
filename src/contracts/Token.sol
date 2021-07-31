5c475c34deaa75b9e227cdc6abf70426a60ef7c2
Neel Gorasiya <mgorasiya1974@gmail.com>
2021-07-31 11:08:56 +0530

    completed first migration check

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,27 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
+  constructor() public payable ERC20("dApp bank currency", "DBC") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dBank) public returns(bool) {
+    require(msg.sender ==  minter, 'Error: msg.sender does not have minter role');
+    minter = dBank;
 
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender ==  minter, 'Error: msg.sender does not have minter role');
 		_mint(account, amount);
 	}
 }
